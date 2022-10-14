9aaaa41080472999b601a916d6d6f8e7b5f8d709
varshap-17 <varshap630@gmail.com>
2022-10-14 16:53:22 +0530

    user dto class

---
+++ src/main/java/com/example/bookstore/dto/UserDto.java
@@ -0,0 +1,29 @@
+package com.example.bookstore.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+public class UserDto {
+    @NotBlank(message = "firstname should not be empty")
+    @Pattern(regexp = "^[A-Z]+[A-Za-z]*$",message = "firstname is invalid")
+    public String firstname;
+    @NotBlank(message = "lastname should not be empty")
+    @Pattern(regexp = "^[a-z]*$",message = "lastname is invalid")
+    public String lastname;
+    @NotBlank(message = "address should not be empty")
+    @Pattern(regexp = "^[A-Za-z]*$")
+    public String address;
+    @NotBlank(message = "email should not be null")
+    @Pattern(regexp = "^[a-z]*@[a-z]*(.)[a-z]*",message ="email is invalid")
+    public String email;
+    @NotBlank(message = "password should not be null")
+    public String password;
+
+    public UserDto(String firstname, String lastname, String address, String email, String password){
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.address=address;
+        this.email=email;
+        this.password=password;
+    }
+}
