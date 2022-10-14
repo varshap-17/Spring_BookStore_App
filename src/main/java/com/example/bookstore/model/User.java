e3661e876dac3dcf345789069d55d12d0675f181
varshap-17 <varshap630@gmail.com>
2022-10-14 16:42:23 +0530

    user model class created

---
+++ src/main/java/com/example/bookstore/model/User.java
@@ -0,0 +1,42 @@
+package com.example.bookstore.model;
+
+import com.example.bookstore.dto.UserDto;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userid;
+    private String firstname;
+    private String lastname;
+    private String address;
+    private String email;
+    private String password;
+
+    public User(){
+
+    }
+    public User(long userid, UserDto userDto){
+        super();
+        this.userid=userid;
+        this.firstname= userDto.firstname;
+        this.lastname= userDto.lastname;
+        this.address= userDto.address;
+        this.email= userDto.email;
+        this.password= userDto.password;
+    }
+    public User(UserDto userDto){
+        this.firstname= userDto.firstname;
+        this.lastname= userDto.lastname;
+        this.address= userDto.address;
+        this.email= userDto.email;
+        this.password= userDto.password;
+    }
+}
