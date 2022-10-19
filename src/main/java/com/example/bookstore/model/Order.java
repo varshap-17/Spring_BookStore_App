16a9a8b3b08a0b7f16aa68e928c7575dbb04f556
varshap-17 <varshap630@gmail.com>
2022-10-19 20:18:04 +0530

    order model class

---
+++ src/main/java/com/example/bookstore/model/Order.java
@@ -0,0 +1,46 @@
+package com.example.bookstore.model;
+
+import com.example.bookstore.dto.OrderDto;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer"})
+@Entity
+@Data
+@NoArgsConstructor
+@ToString
+@Table(name = "order_table")
+public class Order implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    private LocalDate orderDate=LocalDate.now();
+    private Integer price;
+    private boolean cancel;
+    private Integer quantity;
+    private String address;
+    @JoinColumn(name = "cart_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Cart cartId;
+
+//    @OneToMany(fetch = FetchType.LAZY,orphanRemoval = true)
+//    @org.hibernate.annotations.ForeignKey(name = "none")
+//    private List<Cart> carts;
+    public Order(Cart cart, OrderDto orderDto){
+        this.cartId= cart;
+        orderData(orderDto);
+    }
+    public void orderData(OrderDto orderDto){
+        this.address=orderDto.address;
+        this.quantity=orderDto.quantity;
+        this.orderDate=getOrderDate();
+        this.price=getPrice();
+    }
+}
