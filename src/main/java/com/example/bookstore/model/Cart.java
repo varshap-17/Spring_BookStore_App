93bb80a54bcc2ee2a2934fbb0243a353e00bfc4d
varshap-17 <varshap630@gmail.com>
2022-10-19 20:13:52 +0530

    modified cart model class

--- src/main/java/com/example/bookstore/model/Cart.java
+++ src/main/java/com/example/bookstore/model/Cart.java
@@ -1,23 +1,26 @@
 package com.example.bookstore.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
+@JsonIgnoreProperties({"hibernateLazyInitializer"})
 @Entity
 @Data
 @NoArgsConstructor
-public class Cart {
+public class Cart implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @org.hibernate.annotations.ForeignKey(name = "none")
+//    @org.hibernate.annotations.ForeignKey(name = "none")
     private Long cartId;
     @JoinColumn(name = "userid")
-    @OneToOne(cascade ={CascadeType.ALL})
+    @OneToOne(fetch = FetchType.LAZY)
     private User userid; //foreign key
     @JoinColumn(name = "book_id")
-    @ManyToOne(cascade = {CascadeType.ALL})
+    @ManyToOne(fetch = FetchType.LAZY)
     private Book bookId;  //foreign key
     private Integer quantity;
     private Integer totalPrice;
