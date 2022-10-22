6a4c1a022661c07a780df119eab95bf42dc2bef4
varshap-17 <varshap630@gmail.com>
2022-10-22 14:51:45 +0530

    Email model class

---
+++ src/main/java/com/example/bookstore/model/Email.java
@@ -0,0 +1,12 @@
+package com.example.bookstore.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Email {
+    private String to;
+    private String subject;
+    private String body;
+}
