f310169fe9791a364063307af80c594db06096d9
varshap-17 <varshap630@gmail.com>
2022-10-22 14:42:01 +0530

    response dto class modified

--- src/main/java/com/example/bookstore/dto/ResponseDto.java
+++ src/main/java/com/example/bookstore/dto/ResponseDto.java
@@ -3,9 +3,15 @@ package com.example.bookstore.dto;
 public class ResponseDto {
     public String message;
     public Object data;
+    public String token;
 
     public ResponseDto(String message,Object data) {
         this.message = message;
         this.data=data;
+    }
+    public ResponseDto(String message,Object data,String token){
+        this.message=message;
+        this.data=data;
+        this.token=token;
     }
 }
