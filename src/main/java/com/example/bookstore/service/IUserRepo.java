de9c3722575b5d1c0002b4fda58264f6aeef7a12
varshap-17 <varshap630@gmail.com>
2022-10-22 14:44:47 +0530

    service interface modified

--- src/main/java/com/example/bookstore/service/IUserRepo.java
+++ src/main/java/com/example/bookstore/service/IUserRepo.java
@@ -1,16 +1,19 @@
 package com.example.bookstore.service;
 
+import com.example.bookstore.dto.ResponseDto;
 import com.example.bookstore.dto.UserDto;
 import com.example.bookstore.model.User;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface IUserRepo {
-    public User createData(UserDto userDto);
+    public ResponseEntity<ResponseDto> createAccount(UserDto userDto);
     public User updateData(Long userid,UserDto userDto);
     public List<User> retrieveData();
     public User deleteData(Long userid);
     public Optional<User> findById(Long userid);
     public List<User> findFirstname(String firstname);
+    public ResponseEntity<ResponseDto> verify(String token);
 }
