c4e96aae6fe50026024979308de1ca83e440897d
varshap-17 <varshap630@gmail.com>
2022-11-14 13:15:56 +0530

    modified book controller class

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -15,12 +15,20 @@ import java.util.List;
 public class BookController {
     @Autowired
     BookService bookService;
-    //example statement
+    /**
+     * example method
+     * @param cls-request param
+     * @return
+     */
     @GetMapping("/example")
     public String example(@RequestParam String cls){
         return "this is "+cls+" table";
     }
-    //save method
+    /**
+     *Implemented bookSave method to add the book details and save them in database
+     * @param bookDto-passing as request body
+     * @return
+     */
     @PostMapping("/postData")
     public ResponseEntity<ResponseDto> bookSave(@RequestBody BookDto bookDto){
         Book book=null;
@@ -28,38 +36,63 @@ public class BookController {
         ResponseDto responseDto=new ResponseDto("data saved successfully",book);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
-    //retrieve data method
+    /**
+     *Implemented bookRetrieve method to retrieve all details
+     * @return
+     */
     @GetMapping("/getData")
     public List<Book> bookRetrieve(){
         return bookService.retrieve();
     }
-    //update method
+    /**
+     *Implemented bookUpdate method to update them by bookId
+     * @param bookId-passing as path variable
+     * @param bookDto-passing as request body
+     * @return
+     */
     @PutMapping("/putData/{bookId}")
     public ResponseEntity<ResponseDto> bookUpdate(@PathVariable Long bookId,@RequestBody BookDto bookDto){
         Book book=bookService.update(bookId,bookDto);
         ResponseDto responseDto=new ResponseDto("book data updated id: "+bookId+" successfully",book);
         return new ResponseEntity<>(responseDto,HttpStatus.OK);
     }
-    //delete method
+    /**
+     *Implemented bookDelete method by deleting bookId
+     * @param bookId-passing as path variable
+     * @return
+     */
     @DeleteMapping("/delete/{bookId}")
     public ResponseEntity<ResponseDto> bookDelete(@PathVariable Long bookId){
         Book book=bookService.delete(bookId);
         ResponseDto responseDto=new ResponseDto("book data deleted id: "+bookId+" successfully",book);
         return new ResponseEntity<>(responseDto,HttpStatus.OK);
     }
-    //find data by id method
+    /**
+     *Implemented bookById method to get details by bookId
+     * @param bookId
+     * @return
+     */
     @GetMapping("/byId/{bookId}")
     public ResponseEntity<ResponseDto> bookById(@PathVariable Long bookId){
         Book book=bookService.findById(bookId);
         ResponseDto responseDto=new ResponseDto("get data by id: "+bookId+" successfully",book);
         return new ResponseEntity<>(responseDto,HttpStatus.OK);
     }
-    //find data by price in descending order(high to low)
+    /**
+     *Implemented bookHighToLow method to get data by price in descending order(high to low)
+     * @param price
+     * @return
+     */
     @GetMapping("/byPrice")
     public List<Book> bookHighToLow(Integer price){
         return bookService.findByDescending(price);
     }
-    //find data by quantity in ascending order(low to high)
+
+    /**
+     * Implemented booksLowToHigh method to get data by quantity in ascending order(low to high)
+     * @param quantity
+     * @return
+     */
     @GetMapping("/byQuantity")
     public List<Book> booksLowToHigh(Integer quantity){
         return bookService.findByAscending(quantity);
