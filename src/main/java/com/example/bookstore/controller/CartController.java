7ad652230784ad16fff38acf732c5fa36ffe08c5
varshap-17 <varshap630@gmail.com>
2022-11-14 13:17:10 +0530

    modified cart controller class

--- src/main/java/com/example/bookstore/controller/CartController.java
+++ src/main/java/com/example/bookstore/controller/CartController.java
@@ -16,7 +16,12 @@ import java.util.List;
 public class CartController {
     @Autowired
     CartService cartService;
-    //add to cart data saved
+    /**
+     * Implemented cartSave method by giving userId as path and saving cart data
+     * @param userId-passing as path variable
+     * @param cartDto-passing as request body
+     * @return
+     */
     @PostMapping("/postCart/{userId}")
     public ResponseEntity<ResponseDto> cartSave(@PathVariable Long userId,@RequestBody CartDto cartDto){
         Cart cart=null;
@@ -24,45 +29,75 @@ public class CartController {
         ResponseDto responseDto=new ResponseDto("data added successfully",cart);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
-    //update data
+    /**
+     *Implemented cartUpdate method by giving cartId and updating quantity
+     * @param cartId-passing as path variable
+     * @param quantity-passing as param
+     * @return
+     */
     @PutMapping("/putCart/{cartId}")
     public ResponseEntity<ResponseDto> cartUpdate(@PathVariable Long cartId,@RequestParam int quantity){
         Cart cart=cartService.update(cartId,quantity);
         ResponseDto responseDto=new ResponseDto("data updated cartId: "+cartId+" successfully",cart);
         return new ResponseEntity<>(responseDto,HttpStatus.OK);
     }
-    //retrieve data
+    /**
+     *Implemented cartRetrieve method to get all data
+     * @return
+     */
     @GetMapping("/getCart")
     public List<Cart> cartRetrieve(){
         return cartService.retrieve();
     }
-    //delete method
+    /**
+     *Implemented cartDelete method to delete cart by cartId
+     * @param cartId-as path variable
+     * @return
+     */
     @DeleteMapping("/deleteCart/{cartId}")
     public ResponseEntity<ResponseDto> cartDelete(@PathVariable Long cartId){
         Cart cart=cartService.delete(cartId);
         ResponseDto responseDto=new ResponseDto("data deleted id: "+cartId+" successfully",cart);
         return new ResponseEntity<>(responseDto,HttpStatus.OK);
     }
-    //cart by id method
+    /**
+     *Implemented cartById method to get data by cartId
+     * @param cartId-as path variable
+     * @return
+     */
     @GetMapping("/cartById/{cartId}")
     public ResponseEntity<ResponseDto> cartById(@PathVariable Long cartId){
         Cart cart=cartService.findById(cartId);
         ResponseDto responseDto=new ResponseDto("get data by id: "+cartId+" ...",cart);
         return new ResponseEntity<>(responseDto,HttpStatus.OK);
     }
-    //find userid in cart table
+    /**
+     *Implemented findAll method to get all details by giving userid
+     * @param userid-as param
+     * @return
+     */
     @GetMapping("/byUserid/{userid}")
     public List<Cart> findAll(@PathVariable Long userid){
         return cartService.findByUserid(userid);
     }
-    //increasing the quantity
+    /**
+     *Implemented incrementQuantity method to increase quantity by cartId
+     * @param cartId-as path variable
+     * @param quantity-as request param
+     * @return
+     */
     @PutMapping("/increment/{cartId}")
     public ResponseEntity<ResponseDto> incrementQuantity(@PathVariable Long cartId,@RequestParam int quantity){
         Cart cart=cartService.increaseQuantity(cartId,quantity);
         ResponseDto responseDto=new ResponseDto("cart: "+cartId+" is updated..",cart);
         return new ResponseEntity<>(responseDto,HttpStatus.OK);
     }
-    //decreasing the quantity
+    /**
+     *Implemented decrementQuantity method to decreaseQuantity by cartId
+     * @param cartId-as path variable
+     * @param quantity-as request param
+     * @return
+     */
     @PutMapping("/decrement/{cartId}")
     public ResponseEntity<ResponseDto> decrementQuantity(@PathVariable Long cartId,@RequestParam int quantity){
         Cart cart=cartService.decreaseQuantity(cartId,quantity);
