d618ecbe3d88df40cd95483dc9ee7768984e92b3
varshap-17 <varshap630@gmail.com>
2022-11-14 13:16:42 +0530

    modified user controller class

--- src/main/java/com/example/bookstore/controller/UserController.java
+++ src/main/java/com/example/bookstore/controller/UserController.java
@@ -22,17 +22,30 @@ public class UserController {
     UserService userService;
     @Autowired
     TokenUtil tokenUtil;
-    //example statement
+    /**
+     * example method
+     * @param name-as request param
+     * @return
+     */
     @GetMapping("/example")
     public String example(@RequestParam String name){
         return name+" table";
     }
-    //save method
+    /**
+     * Implemented user save method to add and save that user details in database
+     * @param userDto-passed the details in request body
+     * @return
+     */
     @PostMapping("/post")
     public ResponseEntity<ResponseDto> userSave(@Valid @RequestBody UserDto userDto){
         return userService.createAccount(userDto);
     }
-    //update method
+    /**
+     * Implemented user update method by taking token and update other details by userDto
+     * @param token-it is a path variable
+     * @param userDto-passed other details
+     * @return
+     */
     @PutMapping("/update/{token}")
     public ResponseEntity<ResponseDto> userUpdate(@Valid @PathVariable String token,@RequestBody UserDto userDto){
         Long tokenId=tokenUtil.decodeToken(token);
@@ -40,12 +53,19 @@ public class UserController {
         ResponseDto responseDto=new ResponseDto("updated data successfully: "+token,user);
         return new ResponseEntity<ResponseDto>(responseDto,HttpStatus.OK);
     }
-    //retrieve data method
+    /**
+     * Implemented the user retrieve method
+     * @return
+     */
     @GetMapping("/getAll")
     public List<User> userRetrieve(){
         return userService.retrieveData();
     }
-    //delete method
+    /**
+     * Implemented user delete method with the token
+     * @param token-passing token as path variable
+     * @return
+     */
     @DeleteMapping("/delete/{token}")
     public ResponseEntity<ResponseDto> userDelete(@PathVariable String token){
         String tokenId= String.valueOf(tokenUtil.decodeToken(token));
@@ -53,7 +73,11 @@ public class UserController {
         ResponseDto responseDto=new ResponseDto("userid: "+tokenId+" is deleted",user);
         return new ResponseEntity<ResponseDto>(responseDto,HttpStatus.OK);
     }
-    //get data by id method
+    /**
+     * Implemented the userById method to get the user by token
+     * @param token-passing token as path variable
+     * @return
+     */
     @GetMapping("/getId/{token}")
     public ResponseEntity<ResponseDto> userById(@PathVariable String token){
         String tokenId= String.valueOf(tokenUtil.decodeToken(token));
@@ -61,21 +85,33 @@ public class UserController {
         ResponseDto responseDto=new ResponseDto("get details by userid: "+tokenId,user);
         return new ResponseEntity<ResponseDto>(responseDto,HttpStatus.OK);
     }
-    //get data by firstname
+    /**
+     * Implemented the userByName method to get the user by firstname
+     * @param firstname-passing firstname as path variable
+     * @return
+     */
     @GetMapping("/getName/{firstname}")
     public ResponseEntity<ResponseDto> userByName(@PathVariable String firstname){
         List<User> users=userService.findFirstname(firstname);
         ResponseDto responseDto=new ResponseDto("get details by firstname: "+firstname,users);
         return new ResponseEntity<ResponseDto>(responseDto,HttpStatus.OK);
     }
-    //get data by login method
+    /**
+     * Implemented userLogin method to get user details by login dto class which having email id and password
+     * @param loginDto-passing login dto as request body
+     * @return
+     */
     @PostMapping("/login")
     public ResponseEntity<ResponseDto> userLogin(@RequestBody LoginDto loginDto){
         Optional<User> user= Optional.ofNullable(userService.login(loginDto));
         ResponseDto responseDto=new ResponseDto("get all details..."+loginDto,user);
         return new ResponseEntity<>(responseDto,HttpStatus.OK);
     }
-    //verify token
+    /**
+     *Implemented verify user method with the token which is verifying the user
+     * @param token-passing token as path variable
+     * @return
+     */
     @GetMapping("/verify/{token}")
     public ResponseEntity<ResponseDto> verifyUser(@PathVariable String token){
         ResponseEntity<ResponseDto> user=userService.verify(token);
