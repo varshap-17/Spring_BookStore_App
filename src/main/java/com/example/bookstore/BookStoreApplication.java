a534f5567bb3761fcf42694648a1eb27a734571b
varshap-17 <varshap630@gmail.com>
2023-03-10 11:23:23 +0530

    main file

---
+++ src/main/java/com/example/bookstore/BookStoreApplication.java
@@ -0,0 +1,16 @@
+package com.example.bookstore;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class BookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreApplication.class, args);
+        log.info("BookStore app");
+    }
+
+}
